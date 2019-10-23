c6dbe3a25671cdd70a27549dec47e1c79d0be4f3
Kumar82098 <56931055+Kumar82098@users.noreply.github.com>
2019-10-23 22:04:05 +0530

    upload file

---
+++ search.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int a[100],n,i,search;
+	printf("Enter no of elements:\n");
+	scanf("%d",&n);
+	printf("Enter %d integers:\n",n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	printf("Enter the no. to search:\n ");
+	scanf("%d",&search);
+	for(i=0;i<n;i++)
+	{
+	  if(a[i]==search)
+	  {
+		printf("The %d element found at position %d.",search,i+1);
+	    break;
+	  }
+	}
+
+	if(i!=search)
+	printf("The %d element not found. ",search);
+}
+	
+	
